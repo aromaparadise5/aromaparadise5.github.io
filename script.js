042353901d0a7c6d0fef722e607f7903f1aa4521
aromaparadise5 <aromaparadise9@gmail.com>
2026-03-07 17:14:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const candles=document.querySelectorAll(".candle");
 
+window.addEventListener("scroll",()=>{
+
+candles.forEach(c=>{
+
+let top=c.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+c.classList.add("show");
+
+}
+
+});
+
+});
